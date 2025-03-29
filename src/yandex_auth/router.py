777fe6fcca822efa80dee286ac55f248cf09de44
nanoCAT <nanocatr@yandex.ru>
2025-03-29 17:26:39 +0300

    Добавление пользователя с токеном в бд, правки

--- src/yandex_auth/router.py
+++ src/yandex_auth/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Response, Request, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_async_session
-from sqlalchemy import select, insert, update, delete
+from sqlalchemy import select, insert
 import os
 import json
 import requests
@@ -11,18 +11,31 @@ load_dotenv()
 client_id = os.environ.get("YANDEX_CLIENT_ID")
 secret_id = os.environ.get("YANDEX_CLIENT_SECRET")
 
-
-from models.models import InsuranceRate
-
 router = APIRouter()
+from models.models import user
+from user.schemas import User
 
-@router.post("/get_token", summary="Получение токена yandexID", description="Перейдите на https://oauth.yandex.ru/authorize?response_type=code&client_id=11fe71c861384ddd9061e672c2935ffb, выберите аккаунт, скопируйте код.")
+@router.get("/token", summary="Получение токена yandexID", description=f"Перейдите по [адресу своего приложения](https://oauth.yandex.ru/authorize?response_type=code&client_id={client_id}), выберите аккаунт.")
 async def post_auth_token(code: str, response: Response, session : AsyncSession = Depends(get_async_session)):
     req_post = requests.post("https://oauth.yandex.ru/token", data={'grant_type': 'authorization_code', 'code': code, 'client_id': client_id, 'client_secret': secret_id})
     data = json.loads(req_post.text)
     token = data['access_token']
     response.set_cookie(key="yandex_token", value=token)
+    req = requests.get("https://login.yandex.ru/info?", headers={"Authorization":f"OAuth {token}"}, data={"format": "json"}) # Получаем id пользователя для удобства
+    user = User.model_validate(json.loads(req.text))
+    user_id = json.loads(req.text)['id']
+    response.set_cookie(key="user_id", value=user_id)
+    await add_user_to_db(user, session)
     return {"status": "success"}
+
+async def add_user_to_db(data: User, session):
+    data = data.model_dump()
+    query = select(user).where(user.c.id == data["id"])
+    result = await session.execute(query)
+    if not result.scalar():
+        query = insert(user).values(data)
+        await session.execute(query)
+        await session.commit()
 
 async def get_token_cookie(request: Request):
     token = request.cookies.get("yandex_token")
@@ -36,8 +49,9 @@ async def post_addrate(token_cookie: str = Depends(get_token_cookie)):
     user = json.loads(req.text)
     return user
 
-@router.get("/delete_token", summary="Удалить токен")
-async def post_addrate(response: Response, session : AsyncSession = Depends(get_async_session)):
+@router.get("/delete_token", summary="Удалить токен и id текущего пользователя")
+async def post_addrate(response: Response):
     response.delete_cookie(key="yandex_token")
+    response.delete_cookie(key="user_id")
     return {"status": "success"}
 
